45cd0681331777f4dfea841dcb198382868a42e2
M ilham sabar <60200111048@uin-alauddin.ac.id>
2018-12-30 14:26:50 +0800

    initial

---
+++ nuxt.config.js
@@ -0,0 +1,57 @@
+module.exports = {
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: 'M Ilham Sabar',
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-  scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'M Ilham Sabar, Web Developer'
+      }
+    ],
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
+  },
+  modules: [
+    // provide path to the file with resources
+    ['nuxt-sass-resources-loader', './assets/main.scss']
+  ],
+  /*
+   ** Customize the progress bar color
+   */
+  loading: {
+    color: 'rgb(221, 209, 209)'
+  },
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** Run ESLint on save
+     */
+    extend(config, {
+      isDev,
+      isClient
+    }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="hero is-medium is-fullheight">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <div class="has-text-centered">
+          <figure class="image is-flex is-128x128 has-image-centered">
+            <img class="is-rounded" src="~/assets/images/bg.jpg">
+          </figure>
+        </div>
+        <h1 class="title is-1 pt-is-1">Hi I'm Ilham Sabar</h1>
+        <h2 class="subtitle">Web Developer</h2>
+        <div class="is-row">
+          <span class="icon has-text-info">
+            <i class="fas fa-info-circle"></i>
+          </span>
+          <span class="icon has-text-success">
+            <i class="fas fa-check-square"></i>
+          </span>
+          <span class="icon has-text-warning">
+            <i class="fas fa-exclamation-triangle"></i>
+          </span>
+          <span class="icon has-text-danger">
+            <i class="fas fa-ban"></i>
+          </span>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from "~/components/AppLogo.vue";
+
+export default {
+  components: {
+    AppLogo
+  }
+};
+</script>
+
+<style lang="scss">
+.has-image-centered {
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.pt-is-1 {
+  margin-top: 1em;
+}
+</style>
+
